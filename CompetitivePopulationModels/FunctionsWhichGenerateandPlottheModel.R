661990415187e8bbe0240ea76c52d347d3a02130
Ediem Aljibury <ediemaljibury@dhcp-10-249-3-149.eduroam.wireless.private.cam.ac.uk>
2018-03-05 15:49:42 +0000

    with heat map attempt

--- CompetitivePopulationModels/FunctionsWhichGenerateandPlottheModel.R
+++ CompetitivePopulationModels/FunctionsWhichGenerateandPlottheModel.R
@@ -1,5 +1,6 @@
 library(reshape2)
 library(ggplot2)
+library(caTools) 
 
 matrixsummation <- function(m){
   result <- array(sapply(seq_along(m), function(i) {
@@ -11,18 +12,32 @@ return(result)
 }
 
 matrixcreator <-function(){
+  lakeS <- read.csv(file="../MatlabModels/mylake.txt", head=FALSE) 
+
+  
   LikelihoodMap <- list()
-  LikelihoodMap[[1]] <- matrix(0.2, nrow = 10, ncol = 10)
+  LikelihoodMap[[1]] <- data.matrix((lakeS +  matrix(0.5, nrow=12, ncol=12))/10, rownames.force = NA)
+  colnames(LikelihoodMap[[1]]) <- NULL
+  LikelihoodMap[[2]] <- data.matrix((lakeS +  matrix(0.5, nrow=12, ncol=12))/10, rownames.force = NA )
+  colnames(LikelihoodMap[[2]]) <- NULL
+  LikelihoodMap[[3]] <- data.matrix((lakeS +  matrix(0.5, nrow=12, ncol=12))/9, rownames.force = NA )
+  colnames(LikelihoodMap[[3]]) <- NULL
+  LikelihoodMap[[4]] <- matrix(0.01, nrow = 12, ncol = 12)
+  LikelihoodMap[[5]] <- matrix(0.01, nrow = 12, ncol = 12)
+  LikelihoodMap[[6]] <- matrix(0.01, nrow = 12, ncol = 12)
+  
+  LikelihoodMap <- list()
+  LikelihoodMap[[1]] <- matrix(0.1, nrow = 10, ncol = 10)
   LikelihoodMap[[2]] <- matrix(0.1, nrow = 10, ncol = 10)
   LikelihoodMap[[3]] <- matrix(0.1, nrow = 10, ncol = 10)
   LikelihoodMap[[4]] <- matrix(0.01, nrow = 10, ncol = 10)
   LikelihoodMap[[5]] <- matrix(0.01, nrow = 10, ncol = 10)
   LikelihoodMap[[6]] <- matrix(0.01, nrow = 10, ncol = 10)
   
-  StartingMatrix <- matrix(0, nrow = 10, ncol = 10)
-  StartingMatrix[1,1] <- 1 
-  StartingMatrix[1,2] <- 2
-  StartingMatrix[1,3] <- 3
+  StartingMatrix <- matrix(0, nrow = 12, ncol = 12)
+  StartingMatrix[1:2,1] <- 1 
+  StartingMatrix[1:2,5] <- 2
+  StartingMatrix[1:2,10] <- 3
 }
 
 
@@ -35,6 +50,11 @@ modelsimulator <- function(StartingMatrix, LikelihoodMap, NumberofTrials){
   CurrentMatrix = StartingMatrix
   dimensions <- dim(StartingMatrix)
   Ones <- matrix(1, dimensions[1], dimensions[2])
+  
+  melted <- melt(CurrentMatrix)
+  melted$Var0 <- 0
+  Updated <- melted
+  
     for(i in c(1:NumberofTrials)){
 
       FreeSpots <- (CurrentMatrix==0)
@@ -66,11 +86,27 @@ modelsimulator <- function(StartingMatrix, LikelihoodMap, NumberofTrials){
       CurrentMatrix[ RandomNumbers < LikelihoodofDeath]<-0 
       
       print(CurrentMatrix)
-      }
+      #This bit can be commented out to increase efficiency but stops the output of the video
+      melted <- melt(CurrentMatrix)
+      melted$Var0 <- i
+      Updated <- rbind(Updated, melted)
+    }
   
-  return(CurrentMatrix)
+  return <-  list("finalmatrix"=CurrentMatrix, "Updated"=Updated)
+
+  return(return)
 }
 
-modelplotter<- function(){ 
+finalstepmodelplotter<- function(Model){
+  CurrentMatrix <- Model$finalmatrix
   melted_cormat <- melt(CurrentMatrix)
-  ggplot(data = melted_cormat, aes(x=Var1, y=Var2, fill=value)) + geom_tile() }
+  colnames(melted_cormat) <- c(firstindex, secondindex, color)
+  Stuff <- cbind(melted_cormat, melt(data.matrix(lakeS,  rownames.force = NA)))
+  #p <- ggplot(data =  melt(data.matrix(lakeS,  rownames.force = NA)),aes(x=Var1, y=Var2, fill=as.factor(value))) + geom_tile() + theme(legend.position="none")
+
+  ggplot(data = Stuff, aes(x=Var1, y=Var2, fill=color)) + geom_tile() 
+
+  
+  #p + ggplot(data = melted_cormat, aes(x=Var1, y=Var2, fill=as.factor(value))) + geom_tile()  +
+    #scale_fill_manual(values=c(t_col("white", perc=100),t_col("#999999", perc=50), t_col("#E69F00", perc=50), t_col("#56B4E9", perc=50)))  + theme(legend.position="none")
+  }
